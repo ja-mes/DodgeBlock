404364df3c0fa3af66430931477680dadfcf5dfd
James <jamezbrown100@gmail.com>
2017-05-04 17:02:52 -0500

    Don't inc score if player is dead

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -9,21 +9,25 @@ public class ScoreManager : MonoBehaviour {
     public Text scoreText;
 	public float timeBeforeDestruction = 1f;
 	private int score = 0;
-	
-	
+	private bool isDead = false;
 
 	public void IncScore(int amount = 1) {
-		score += amount;
-		scoreText.text = score.ToString();
-	}
+        if (!isDead) {
+            score += amount;
+            scoreText.text = score.ToString();
+        }
+    }
 
-	public void Die() {
+    public void Die()
+    {
+        isDead = true;
         Destroy(GameObject.FindGameObjectWithTag("Player"));
-		Invoke("ResetScene", timeBeforeDestruction);
-		GameObject.FindObjectOfType<GlobalScript>().score = score;
-	}
+        Invoke("ResetScene", timeBeforeDestruction);
+        GameObject.FindObjectOfType<GlobalScript>().score = score;
+    }
 
-	private void ResetScene() {
-		SceneManager.LoadScene(2);
-	}
+    private void ResetScene()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
