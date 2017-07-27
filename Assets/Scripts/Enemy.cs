ce374fa7f7a0275c30e3892807fab47aefac6f09
James <jamezbrown100@gmail.com>
2017-07-27 15:16:15 -0500

    Close #3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public ParticleSystem part;
+
     bool hasHitScore = false;
 
     void Start()
@@ -22,13 +24,27 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void InvokeExplosion()
+    {
+        ParticleSystem newPart = Instantiate(part, transform.position, Quaternion.identity);
+        newPart.Play();
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Player" && !Globals.GM.playerHasShield)
+        if (col.gameObject.tag == "Player")
         {
-            Globals.Player.Die();
-            col.gameObject.GetComponent<Player>().InvokeExplosion();
+            if (Globals.GM.playerHasShield)
+            {
+                Globals.Player.IncScore(10);
+                InvokeExplosion();
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Globals.Player.Die();
+                col.gameObject.GetComponent<Player>().InvokeExplosion();
+            }
         }
     }
 
