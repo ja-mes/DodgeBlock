619d886adef06edd905ac9cb4d036b08ec359375
James <jamezbrown100@gmail.com>
2017-08-13 16:29:43 -0500

    Destroy PS clones after 6 seconds

---
+++ Assets/Scripts/PSDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSDestruct : MonoBehaviour
+{
+	public float destructionTime = 6;
+
+	void Update()
+	{
+		destructionTime -= Time.deltaTime;
+
+		if (destructionTime <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
